0ad32e66deb25ddf4d85a91bcfff125bb4ca5d6c
m447588103-design <m447588103@gmail.com>
2025-10-28 20:31:39 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 
 
 URLS_LIKE = {"IND":"https://client.ind.freefiremobile.com/LikeProfile",
-            "BR":"https://client.us.freefiremobile.com/LikeProfile",
+            "BD":"https://client.us.freefiremobile.com/LikeProfile",
             "US":"https://client.us.freefiremobile.com/LikeProfile",
             "SAC":"https://client.us.freefiremobile.com/LikeProfile",
             "NA":"https://client.us.freefiremobile.com/LikeProfile"}
 
 
 URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow",
-            "BR":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+            "BD":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "US":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "SAC":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
             "NA":"https://client.us.freefiremobile.com/GetPlayerPersonalShow"}
@@ -16,7 +16,7 @@ URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow"
 
 
 FILES = {"IND":"token_ind.json",
-             "BR":"token_br.json",
+             "BD":"token_bd.json",
              "US":"token_br.json",
              "SAC":"token_br.json",
              "NA":"token_br.json"}
